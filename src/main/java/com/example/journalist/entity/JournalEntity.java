042d322239cf49d28a6bbf2131f6528413ec9a21
Asim Ansari <masimansari237@gmail.com>
2025-03-15 00:39:43 +0500

    Using lombok we can optimized code.

--- src/main/java/com/example/journalist/entity/JournalEntity.java
+++ src/main/java/com/example/journalist/entity/JournalEntity.java
@@ -1,11 +1,15 @@
 package com.example.journalist.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
 @Document(collection =  "journal_entries")
 public class JournalEntity {
     @Id
@@ -14,35 +18,4 @@ public class JournalEntity {
     private String content;
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
